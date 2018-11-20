3d07717d4148a27bafcee13621a3d9945dbbccdd
konovalovaea <f-lestern@mail.ru>
2018-11-20 07:51:53 +0300

    123

--- src/lesson1/task11/Main.java
+++ src/lesson1/task11/Main.java
@@ -13,9 +13,13 @@ public class Main {
         int[][] array = {{1, 2, 3}, {4, 5, 6,}, {7, 8, 9,}};
     }
 
-    public static void sort() {
+    public static void sort(int[] array) {
 
-    }
+        }
 
-}
+
+        }
+
+
+
 
--- src/lesson1/task12/Main.java
+++ src/lesson1/task12/Main.java
@@ -1,4 +1,5 @@
 package lesson1.task12;
+
 /**
  * Напишите метод который принимает математическое выражение в формате String и возвращает результат.
  * В выражении используются только целые числа, поддерживаемые операции:+,-.
@@ -8,16 +9,32 @@ package lesson1.task12;
  * 	math(" 2-  -1"); // вернёт [3]
  */
 public class Main {
-public static void main (){
- //   System.out.print(math("2-1"));
- ///   System.out.print(math("2- +1"));
-    //System.out.print(math("2-  -1"));
+public static void main (String args[]){
+     System.out.print(Function("2-1"));
+     System.out.print(Function("2- +1"));
+     System.out.print(Function("2-  -1"));
 }
 
-private static void math (String str){
+    private static Integer Function(String str) {
+     int i = 0;
+     int num = 0;
+     boolean isNeg = false;
 
+        if (str.charAt(0) == '-') {
+            isNeg = true;
+            i = 1;
+        }
 
-}
+        while( i < str.length()) {
+            num *= 10;
+            num += str.charAt(i++) - '0';
+        }
+
+        if (isNeg)
+            num = -num;
+        return num;
+    }
+
 
 
 }
