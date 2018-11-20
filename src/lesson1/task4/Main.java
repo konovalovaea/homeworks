6b27795f49860841ec10af3b653729709992ed08
konovalovaea <f-lestern@mail.ru>
2018-11-20 07:51:18 +0300

    123

--- src/lesson1/task4/Main.java
+++ src/lesson1/task4/Main.java
@@ -1,17 +1,28 @@
 package lesson1.task4;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 2, 3, 4, 3, 2, 5};
+        int n = input.length;
+
+        for (int i = 0, m = 0; i != n; i++, n = m) {
+            for (int j = m = i + 1; j != n; j++) {
+                if (input[j] != input[i]) {
+                    if (m != j) input[m] = input[j];
+                    m++;
+                }
+            }
+        }
+
+
+        if (n != input.length) {
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++) b[i] = input[i];
+
+            input = b;
+        }
+
+        for (int x : input) System.out.print(x + " ");
         System.out.println();
     }
-
-    public static void sort() {
-
-    }
-
-
-
-
-
-
 }
